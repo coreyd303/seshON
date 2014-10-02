0d7190aa2327468b13440d2f37196c6d0326ef4e
Corey Davis <coreyd303@gmail.com>
2014-10-01 18:42:15 -0600

    css for homepage

--- config/initializers/omniauth.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :foursquare, "ZPIPTWQFHU0QSL1E5JD5IO5APOHUTZI3YKKISR0ANBGPQYGO", "HSH2BCXQRMJSR3KF12O4QLS12M5TSUD5E3NQCGFSZN45HVGX", {provider_ignores_state: true}
-end
