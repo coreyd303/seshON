18ae9e2b9d3ee63a4e00a75bd8cda787af23b499
Corey Davis <coreyd303@gmail.com>
2014-10-15 11:18:55 -0600

    updates seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first
+
+user = User.create {
+  name: "CB Davis",
+  provider: "facebook",
+  uid: "296826253842981",
+  email: "coreyd303@gmail.com",
+  oauth_token: nil,
+  role: "admin"
+}
