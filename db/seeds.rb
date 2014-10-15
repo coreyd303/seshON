980f0b1b7632673591969b9118fc8a558da4daba
Corey Davis <coreyd303@gmail.com>
2014-10-15 11:22:34 -0600

    fixes shit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,7 @@
 
-user = User.create {
-  name: "CB Davis",
-  provider: "facebook",
-  uid: "296826253842981",
-  email: "coreyd303@gmail.com",
-  oauth_token: nil,
-  role: "admin"
-}
+user = User.create(name: "CB Davis",
+                    provider: "facebook",
+                    uid: "296826253842981",
+                    email: "coreyd303@gmail.com",
+                    oauth_token: nil,
+                    role: "admin")
